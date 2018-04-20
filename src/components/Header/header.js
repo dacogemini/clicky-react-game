f3f8834829d2be4a2d56d7f164253880752c5a2e
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2018-04-20 14:23:12 -0500

    changed index.css

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './header.css'
 
 const Header = props => (
-
+    <center>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark bg-primary fixed top">   
     <div className="gameScore">
         <h4 className="userStatus nav-link-right">Your Score: <span>{props.score}</span> | Top Score: {props.topScore} | <span> {props.status}</span></h4>
     </div>
     </nav>
+    </center>
 );
 
 
